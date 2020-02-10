074959c323944f991920cf01fffa15c524766597
Ivan <suminski@umich.edu>
2020-02-09 19:58:27 -0500

    Adding hw4_git

---
+++ hw4_git.py
@@ -0,0 +1,8 @@
+## just a friendly reminder of some of the commands available...
+print('COMMANDS FOR USING GITHUB')
+print('1) git init : initializes git repository (local)')
+print('2) git status : shows status of local repository')
+print('3) git add <filename> : adds file to repository')
+print('4) git commit -m : -m attaches message to snapshot')
+print('5) git remote add origin + <link> : link is to repository on gitHub')
+print('6) git push -u origin master : push to gitHub (-u) makes this repository default')
